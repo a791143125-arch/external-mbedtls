47c809a6d4565594db3d065834aec265321afed5
Simon Morlat <simon.morlat@linphone.org>
2021-10-05 18:03:53 +0200

    Fix an issue causing wrongly "not-trusted" certificate chains during tls handshake. The issue occurs only with clang 12 (randomly) and 13 (consistently), on aarch64 and x86_64 (not verified for other archs).
    It is documented here : https://github.com/ARMmbed/mbedtls/issues/4786#issuecomment-913599094

--- library/bignum.c
+++ library/bignum.c
@@ -1535,13 +1535,13 @@ int mbedtls_mpi_sub_int( mbedtls_mpi *X, const mbedtls_mpi *A, mbedtls_mpi_sint 
  * Helper for mbedtls_mpi multiplication
  */
 static
-#if defined(__APPLE__) && defined(__arm__)
+//#if defined(__APPLE__) && defined(__arm__)
 /*
  * Apple LLVM version 4.2 (clang-425.0.24) (based on LLVM 3.2svn)
  * appears to need this to prevent bad ARM code generation at -O3.
  */
 __attribute__ ((noinline))
-#endif
+//#endif
 void mpi_mul_hlp( size_t i, mbedtls_mpi_uint *s, mbedtls_mpi_uint *d, mbedtls_mpi_uint b )
 {
     mbedtls_mpi_uint c = 0, t = 0;
